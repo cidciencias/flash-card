e4eccac23d0b7f287203d4d3fcf7f04121f6396d
Luis Cid <alexcid2002@gmail.com>
2025-03-13 12:47:49 +0000

    V1.1
    Added base GUI, with the card itself, 2 labels and 2 buttons

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(height=526, width=800)
+frond_card_img = PhotoImage(file="images/card_front.png")
+back_card_img = PhotoImage(file="images/card_back.png")
+canvas.create_image(400, 263, image=frond_card_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+# Buttons
+wrong_img = PhotoImage(file="images/wrong.png")
+right_img = PhotoImage(file="images/right.png")
+
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+right_button = Button(image=right_img, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+
+window.mainloop()
