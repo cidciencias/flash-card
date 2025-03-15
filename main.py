a13b99160c2e206b0cf20e031971c5fabdb041de
Luis Cid <alexcid2002@gmail.com>
2025-03-15 18:59:17 +0000

    V1.2
    Flipping functionality implemented. After 3 seconds user can now see whats the meaning of the word in English

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=frond_card_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_card_img)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -8,12 +32,14 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(height=526, width=800)
 frond_card_img = PhotoImage(file="images/card_front.png")
 back_card_img = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=frond_card_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_background = canvas.create_image(400, 263, image=frond_card_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -23,10 +49,11 @@ canvas.grid(row=0, column=0, columnspan=2)
 wrong_img = PhotoImage(file="images/wrong.png")
 right_img = PhotoImage(file="images/right.png")
 
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
